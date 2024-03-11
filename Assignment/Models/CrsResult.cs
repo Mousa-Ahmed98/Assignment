eb3d94b1f0ba057ceb4f3fb48965f040761c49fd
Mousa <54138093+Mousa-Ahmed98@users.noreply.github.com>
2024-03-12 00:18:43 +0200

    Database Inistantiated

---
+++ Assignment/Models/CrsResult.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Assignment.Models
+{
+    [Table("CrsResult")]
+    public class CrsResult
+    {
+        public int Id { get; set; }
+        public int Degree { get; set; }
+
+        
+        public int CourseId { get; set; }
+
+        public int TraineeId { get; set; }
+
+
+        public Course Course { get; set; }
+        public Trainee Trainee { get; set; }
+    }
+}
---
+++ Assignment/Models/Department.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Assignment.Models
+{
+    [Table("Department")]
+    public class Department
+    {
+        public int Id {  get; set; }
+        public string Name { get; set; }
+        public string ManagerName { get; set; }
+
+
+        public List<Instructor> Instructors { get; set; }
+        public List<Course> Courses { get; set; }
+        public List<Trainee> Trainee { get; set; }
+    }
+}
